ceb03ea7082a2a8764532e2803afe0e6b47cc48c
John Garrett <infantrygarrett@icloud.com>
2019-04-07 21:39:42 -0500

    update server js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//added for images
+app.use(express.static(path.join("app","public")));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
 
