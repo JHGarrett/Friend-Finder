6414fb3d2f3f1a10467bbb2fba25f039137c5bc5
John Garrett <infantrygarrett@icloud.com>
2019-04-08 20:53:56 -0500

    added some friends

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+var friends = [
+    {
+      name: 'John',
+      profilePic: "../images/johnfunny.jpg",
+      scores: [
+          "4",
+          "5",
+          "5",
+          "2",
+          "4",
+          "5",
+          "4",
+          "4",
+          "3",
+          "4"
+        ]
+    },
+    {
+        name:"Ted Mosby",
+        photo:"../images/tedmosby.jpg",
+        scores:[            
+            '5',
+            '1',
+            '4',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '4',
+            '1'
+        ]
+    }, 
+    {
+        name:"Barney Stinson",
+        photo:"../images/Barney-Stinson-1.jpg",
+        scores:[            
+            '4',
+            '3',
+            '4',
+            '1',
+            '5',
+            '2',
+            '3',
+            '5',
+            '4',
+            '1'
+        ]
+    }, 
+    {
+        name:"Marshall Eriksen",
+        photo:"../images/marshall-eriksen.jpeg",
+        scores:[            
+            '1',
+            '4',
+            '2',
+            '3',
+            '2',
+            '1',
+            '3',
+            '2',
+            '2',
+            '1'
+        ]
+    }
+
+
+  ]
+  
+  module.exports = friends;
